739c42d89f806d03dd1dc9fcb7689270cfebd9ed
he390291139 <390291139@qq.com>
2019-05-01 13:04:44 +0800

    .

--- application/index/controller/Base.php
+++ application/index/controller/Base.php
@@ -1,15 +1,16 @@
 <?php
 namespace app\index\controller;
-<<<<<<< HEAD
 use think\Controller;
-use think\Request;
-use think\auth\Auth;
-=======
+// <<<<<<< HEAD
+// use think\Controller;
+// use think\Request;
+// use think\auth\Auth;
+// =======
 
-use think\Controller;
-use think\Request;
+// use think\Controller;
+// use think\Request;
 
->>>>>>> 85b82e344cc9239d079c3a987739fd0c49661dba
+// >>>>>>> 85b82e344cc9239d079c3a987739fd0c49661dba
 class Base extends Controller
 {
     protected $model;

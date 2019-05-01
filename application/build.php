25ff25a197de354d28f5aa50226b004d17787468
heihei <ljjopq123@163.com>
2019-05-01 12:50:38 +0800

    搜索

--- application/build.php
+++ application/build.php
@@ -14,8 +14,8 @@ return [
     '__file__' => ['common.php', 'config.php', 'database.php'],
 
     // 定义demo模块的自动生成 （按照实际定义的文件名生成）
-    'admin'     => [
-        'view'       => ['user/index','user/add','user/edit'],
+    'index'     => [
+        'view'       => ['index/search'],
     ],
     // 其他更多的模块定义
 ];
--- application/index/controller/Base.php
+++ application/index/controller/Base.php
@@ -5,10 +5,7 @@ use think\Controller;
 use think\Request;
 
 class Base extends Controller
-{
-    protected $model;
-    
-    protected $auth;
+{    
 
     public function _initialize(){
 

e94daa4fc251d959db5ca27ae506157bb6aaabe8
heihei <ljjopq123@163.com>
2019-05-01 20:36:56 +0800

    。。。

--- application/index/controller/Base.php
+++ application/index/controller/Base.php
@@ -1,15 +1,8 @@
 <?php
 namespace app\index\controller;
-<<<<<<< HEAD
 use think\Controller;
 use think\Request;
 use think\auth\Auth;
-=======
-
-use think\Controller;
-use think\Request;
-
->>>>>>> 85b82e344cc9239d079c3a987739fd0c49661dba
 class Base extends Controller
 {    
 

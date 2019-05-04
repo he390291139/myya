a23683c947e537946e6db2612a90a488d3b9640a
heihei <ljjopq123@163.com>
2019-05-05 00:02:31 +0800

    医案管理基本完成

--- application/admin/controller/Ya.php
+++ application/admin/controller/Ya.php
@@ -8,7 +8,7 @@ use app\admin\model\Doctor;
 
 class Ya extends Base
 {
-    protected $pageNum = "20";
+    protected $pageNum = "5";
 
     public function _initialize()
     {
@@ -16,9 +16,9 @@ class Ya extends Base
         $this->model = new yian();
     }
 
-    public function index()
+    public function index($key =null)
     {
-        $list = $this->model->with(['doctor'=>function($query){
+        $list = $this->model->with(['doctor' => function ($query) {
             $query->withField('name');
             $query->field('name');
         }])->paginate($this->pageNum);
@@ -33,9 +33,63 @@ class Ya extends Base
     public function add()
     {
         $doc_list = Doctor::field('id,name')->select();
-        $this->assign('doc_list',$doc_list);
-        dump($doc_list);
-        die();
+        $this->assign('doc_list', $doc_list);
+
+        if ($this->request->isPost()) 
+        {
+            $param = $this->request->param();
+            // dump($param);
+            // die();
+            global $imgUrl;
+            global $imgerror;
+            global $videoUrl;
+            global $videoerror;
+            // 获取表单上传文件
+            if(!$param['imgs'] == ''){
+                $file = request()->file('imgs');
+                // 移动到框架应用根目录/public/uploads/ 目录下
+                $info = $file->validate(['ext'=>'jpg,png,gif'])->move(ROOT_PATH . 'public' . DS . 'uploads');
+
+                if($info){
+                    // 成功上传后 获取上传信息
+                     $imgUrl =  $info->getSaveName();
+                }else{
+                    // 上传失败获取错误信息
+                    $imgerror =  $file->getError();
+                }
+                $param['imgs'] = $imgUrl;
+            }
+
+            if(!$param['video'] == ''){
+                $file = request()->file('video');
+                // 移动到框架应用根目录/public/uploads/ 目录下
+                $info = $file->validate(['ext'=>'mp4'])->move(ROOT_PATH . 'public' . DS . 'uploads');
+
+                if($info){
+                    // 成功上传后 获取上传信息
+                     $videoUrl =  $info->getSaveName();
+                }else{
+                    // 上传失败获取错误信息
+                    $videoerror =  $file->getError();
+                }
+                $param['video'] = $videoUrl;
+            }
+            if (!is_null($imgerror))
+            {
+                return json("图片错误：$imgerror");
+            }
+            if (!is_null($videoerror))
+            {
+                return json("视频错误：$videoerror");
+            }
+            $res = $this->model->save($param);
+            if($res)
+                return json(true);
+            else
+                return json(false);
+
+        }
+
         return $this->view->fetch();
     }
 
@@ -45,9 +99,75 @@ class Ya extends Base
      * @param  int  $id
      * @return \think\Response
      */
-    public function edit($id)
+    public function edit($id = null)
     {
-        //
+        if($id == null)
+            return json(false);
+        $list = $this->model->get($id);
+        $this->assign('user',$list);
+        $doc_list = Doctor::field('id,name')->select();
+        $this->assign('doc_list', $doc_list);
+        
+        if ($this->request->isPost()) 
+        {
+            $param = $this->request->param();
+            // dump($param);
+            // die();
+            global $imgUrl;
+            global $imgerror;
+            global $videoUrl;
+            global $videoerror;
+            // 获取表单上传文件
+            if(!$param['imgs'] == ''){
+                $file = request()->file('imgs');
+                // 移动到框架应用根目录/public/uploads/ 目录下
+                $info = $file->validate(['ext'=>'jpg,png,gif'])->move(ROOT_PATH . 'public' . DS . 'uploads');
+
+                if($info){
+                    // 成功上传后 获取上传信息
+                     $imgUrl =  $info->getSaveName();
+                }else{
+                    // 上传失败获取错误信息
+                    $imgerror =  $file->getError();
+                }
+                $param['imgs'] = $imgUrl;
+            }else{
+                $param['imgs'] = $list->imgs;
+            }
+
+            if(!$param['video'] == ''){
+                $file = request()->file('video');
+                // 移动到框架应用根目录/public/uploads/ 目录下
+                $info = $file->validate(['ext'=>'mp4'])->move(ROOT_PATH . 'public' . DS . 'uploads');
+
+                if($info){
+                    // 成功上传后 获取上传信息
+                     $videoUrl =  $info->getSaveName();
+                }else{
+                    // 上传失败获取错误信息
+                    $videoerror =  $file->getError();
+                }
+                $param['video'] = $videoUrl;
+            }else{
+                $param['video'] = $list->video;
+            }
+            
+            if (!is_null($imgerror))
+            {
+                return json($imgerror);
+            }
+            if (!is_null($videoerror))
+            {
+                return json($videoerror);
+            }
+            $res = $this->model->save($param,['id'=>$param['id']]);
+            if($res)
+                return json(true);
+            else
+                return json(false);
+
+        }
+        return $this->fetch();
     }
 
 
@@ -57,11 +177,11 @@ class Ya extends Base
      * @param  int  $id
      * @return \think\Response
      */
-    public function delete($ids =null)
+    public function delete($ids = null)
     {
-        if($this->request->isPost()){
+        if ($this->request->isPost()) {
             $res = $this->model->destroy($ids);
-            if($res)
+            if ($res)
                 return json(true);
             else
                 return json(false);
